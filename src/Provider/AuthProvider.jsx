b1253d3816648df5c7eeb7c63655576b6c9c5c30
SM-junior <shahinalam12341@gmail.com>
2024-02-11 15:55:29 +0600

    axios install, import is done and working well and axios.post, normal fetch is woriking well

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -2,6 +2,7 @@ import { createContext, useEffect, useState } from "react";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, signOut, updateProfile, signInWithPopup } from "firebase/auth";
 import { app } from '../firebase/firebase.config';
 import { GoogleAuthProvider } from "firebase/auth";
+import axios from 'axios';
 
 const provider = new GoogleAuthProvider();
 
@@ -25,7 +26,7 @@ const AuthProvider = ({ children }) => {
     }
 
     //googleLogin
-    const googleLogin=()=>{
+    const googleLogin = () => {
         setLoading(true)
         return signInWithPopup(auth, provider)
     }
@@ -46,6 +47,34 @@ const AuthProvider = ({ children }) => {
     useEffect(() => {
         const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
             setUser(currentUser)
+            //.................using axios...............
+            // if (currentUser) {
+            //     axios.post('http://localhost:3000/jwt', { email: currentUser.email })
+            //         .then(data => {
+            //             console.log(data)
+            //             localStorage.setItem('jwt-access-token', data.data.token)
+            //         })
+            // }
+
+            //................ OR ...............
+            //.......using normal fetch.........
+
+            if(currentUser){
+                fetch('http://localhost:3000/jwt',{
+                    method:'POST',
+                    headers:{
+                        'content-type':'application/json'
+                    },
+                    body:JSON.stringify({email: currentUser.email})
+                })
+                .then(res=>res.json())
+                .then(data=>{
+                    localStorage.setItem('jwt-access-token', data.token)
+                })
+            }
+            else{
+                localStorage.removeItem('jwt-access-token')
+            }
             setLoading(false)
         })
         return () => {
